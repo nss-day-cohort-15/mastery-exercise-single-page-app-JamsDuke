714d7d98c0427a445578ed556b09ba14cd6d42c5
Jamie Duke <jamsduke@gmail.com>
2016-08-15 11:02:00 -0500

    styled car cards

--- carLot.js
+++ carLot.js
@@ -1,16 +1,20 @@
-var CarLot = (function lotModule(carLot = {}) {
-  carLot = [];
+var CarLot = (function () {
+  usedLot = [];
 
-  carLot.getInventory = function getCars(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'inventory.json');
-    xhr.addEventListener('load', function onLoad() {
-      carLot = JSON.parse(xhr.responseText);
-      console.info('Success: Loaded songs from songs.json.');
-      callback(carLot);
+  return {
+    loadInventory: function (populatePage) {
+      var inventoryLoader = new XMLHttpRequest();
+      inventoryLoader.open('GET', 'inventory.json');
+      inventoryLoader.addEventListener('load', function onLoad() {
+        usedLot = JSON.parse(inventoryLoader.responseText).cars;
+        console.info('Success: Loaded inventory.json.');
+        populatePage(usedLot);
     });
-    xhr.send();
+    inventoryLoader.send();
+  },
+  getInventory: function() {
+    return usedLot
   }
+}
 
-  return carLot;
 })(CarLot);
--- quiz.js
+++ quiz.js
@@ -1,18 +1,24 @@
-// var CarLot = (function populatePage(inventory = {}) {
-//   // Loop over the inventory and populate the page
-//   var inventory;
-//   carLot.getInventory(load);
-//   function load(inv) {
-//     inventory = inv.inventory;
-//     for (var i = 0; i < inventory.length; i++) {
-//       inventory.addInvToDom(i, inventory[i]);
-//     }
-//   }
+  // Loop over the inventory and populate the page
+  function populatePage(inventory) {
+    var showRoom = document.querySelector('#showRoom')
+    // console.log(inventory)
+    for (var i = 0; i < inventory.length; i++) {
+      console.log(inventory[i].make)
+      var car = inventory[i]
+      //string interpolation
+      showRoom.innerHTML += `<div class="col-sm-4 carCard" style="border-color: ${car.color}">
+      <img src="${car.url}">
+      <h3>${car.year} ${car.make} ${car.model}</h3>
+      <h3>$${car.price}</h3>
+      <h3>${car.color}</h3>
+      <h3>${car.purchased}</h3>
+      <p>${car.description}</p>
+      </div>`
+    }
+CarLot.activateEvents();
+  }
 
-//   // Now that the DOM is loaded, establish all the event listeners needed
-//   CarLot.activateEvents();
-// })
-
-// // Load the inventory and send a callback function to be
-// // invoked after the process is complete
-// CarLot.loadInventory();
+  // Now that the DOM is loaded, establish all the event listeners needed
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
+CarLot.loadInventory(populatePage);
