d33c7582c44410a3f0f41c43d4c7e17e016dbff4
Jamie Duke <jamsduke@gmail.com>
2016-08-16 15:32:45 -0500

    added boostrap row to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,20 +1,29 @@
   // Loop over the inventory and populate the page
 function populatePage(inventory) {
   var showRoom = document.querySelector('#showRoom')
+  var outputString = ""
   // console.log(inventory)
   for (var i = 0; i < inventory.length; i++) {
     console.log(inventory[i].make)
     var car = inventory[i]
+    if (i % 3 === 0) {
+      outputString += `<div class="row">`
+    }
+
     //string interpolation
-    showRoom.innerHTML += `<div class="col-md-4 carCard" style="border-color: ${car.color}">
+    outputString += `<div class="col-md-4 carCard" style="border-color: ${car.color}">
     <img src="${car.url}">
     <h3>${car.year} ${car.make} ${car.model}</h3>
     <h3>$${car.price}</h3>
     <h3>${car.color}</h3>
     <h3>${car.purchased}</h3>
-    <p>${car.description}</p>
+    <p class="enteredText">${car.description}</p>
     </div>`
   }
+  if ((i + 1) % 3 === 0) {
+    outputString += `</div>`
+  }
+showRoom.innerHTML = outputString
 activateEvents();
 }
 
