c6bc3ba04770fbedd297112e4c8f3a6b611e0073
Jamie Duke <jamsduke@gmail.com>
2016-08-16 14:47:23 -0500

    added event listeners and they work!

--- events.js
+++ events.js
@@ -1,35 +1,47 @@
-// When you click on one of the car elements,
-// change the width of the border to a higher value,
-// and change the background color to any other color of your
-// choosing.
+//1. Make a function that adds the "clickedCarCard" class to any carCard that is clicked.
 
-// Also, on click of the car element,
-// clear the value of the text input in the navbar,
-// and put the cursor in the text input.
+//2. Also, on click of the car element, clear the value of the text input in the navbar,
+//and put the cursor in the text input.
 
-// When you start typing into the navbar's text input,
+//3.When you start typing into the navbar's text input,
 // the description of the currently selected car should be
 // bound to what you are typing in and match it exactly.
 
-//IIFE that uses a function that creates all event handlers,
+//4.IIFE that uses a function that creates all event handlers,
 //name it activateEvents
 
-//Final IIFE sets and resets border thickness and background
-//on carCards
+// click on car element in DOM
+function activateEvents () {
+  var userInput = document.getElementById("userInput");
+  var clickListener = document.getElementsByClassName("carCard");
+  var textLocation = null;
+// listener is added here
+  for (var i = 0; i < clickListener.length; i++) {
+    var currentCard = clickListener[i];
+    currentCard.addEventListener("click", function (event){
+      var cardSelected = event.currentTarget;
+      userInput.focus();
+      userInput.value = ("");
 
-var CarLot = (function (highlightCard) {
-
-  highlightCard.addClickedCarCardClass = function () {
-    var allCards = document.querySelectorAll(".carCard");
-
-    for (var i = allMyCarCards.length - 1; i >= 0; i--){
-    allCards[i].classList.remove("clickedCarCard")
+      textLocation = cardSelected.getElementsByClassName("enteredText")[0];
+  restoreState(clickListener, cardSelected);
+  changeState(clickListener, cardSelected)
+    })
+  }
+  userInput.addEventListener("keyup", function(){
+    if (textLocation !== null) {
+      textLocation.innerHTML = userInput.value;
     }
+  })
+}
+// function that adds class that changes border and bg color
+function changeState(clickListener, cardSelected){
+  cardSelected.classList.add("clickedCarCard");
+};
+// function that removes the class that changes border and bg color
+function restoreState(clickListener, cardSelected){
+  for (var i = 0; i < clickListener.length; i++){
+    clickListener[i].classList.remove("clickedCarCard");
+    clickListener[i].classList.add("col-md-4")
   }
-
-  highlightCard.removeClickedCarCardClass = function (targetCard) {
-    currentCarCard.classList.add("clickedCarCard");
-  }
-
-  return highlightCard
-})(CarLot);
+};
--- quiz.js
+++ quiz.js
@@ -1,22 +1,22 @@
   // Loop over the inventory and populate the page
-  function populatePage(inventory) {
-    var showRoom = document.querySelector('#showRoom')
-    // console.log(inventory)
-    for (var i = 0; i < inventory.length; i++) {
-      console.log(inventory[i].make)
-      var car = inventory[i]
-      //string interpolation
-      showRoom.innerHTML += `<div class="col-md-4 carCard" style="border-color: ${car.color}">
-      <img src="${car.url}">
-      <h3>${car.year} ${car.make} ${car.model}</h3>
-      <h3>$${car.price}</h3>
-      <h3>${car.color}</h3>
-      <h3>${car.purchased}</h3>
-      <p>${car.description}</p>
-      </div>`
-    }
-CarLot.activateEvents();
+function populatePage(inventory) {
+  var showRoom = document.querySelector('#showRoom')
+  // console.log(inventory)
+  for (var i = 0; i < inventory.length; i++) {
+    console.log(inventory[i].make)
+    var car = inventory[i]
+    //string interpolation
+    showRoom.innerHTML += `<div class="col-md-4 carCard" style="border-color: ${car.color}">
+    <img src="${car.url}">
+    <h3>${car.year} ${car.make} ${car.model}</h3>
+    <h3>$${car.price}</h3>
+    <h3>${car.color}</h3>
+    <h3>${car.purchased}</h3>
+    <p>${car.description}</p>
+    </div>`
   }
+activateEvents();
+}
 
   // Now that the DOM is loaded, establish all the event listeners needed
 // Load the inventory and send a callback function to be
